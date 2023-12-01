afa5e4f846c36aaa018d7e0be020535aefe06a68
Thomas Do <Thomas Do@DESKTOP-09IBRN8>
2023-11-30 23:12:35 -0500

    Main Menu

--- src/main/java/edu/wit/cs/comp1050/javafx_maven_example/App.java
+++ src/main/java/edu/wit/cs/comp1050/javafx_maven_example/App.java
@@ -1,66 +1,45 @@
 package edu.wit.cs.comp1050.javafx_maven_example;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+		try {
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        String loc = fxml + ".fxml";
-        System.out.printf("Provided: %s%n",loc);
-       
-        URL locfxml = App.class.getResource(loc);
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			stage.setTitle("Into The Abyss");
+			stage.setScene(new Scene(root, 800, 600));
+			stage.show();
+			stage.setOnCloseRequest(event -> quit(stage));
+		} catch (Exception e) {
+			e.printStackTrace();
 
-        //handle a null locfxml if in a jar
-        if (locfxml == null) {
-            String jar_res = App.class.getPackageName().replace('.','/');
-            jar_res = '/' + jar_res + '/'; 
-        	loc = "/resources"+jar_res+loc;
-        	locfxml = App.class.getResource(loc);
-        	if (locfxml == null)
-        		System.out.printf("Error: loading fxml with value of '%s'.",loc);
-        }
-       
-        System.out.printf("Loading: %s%n",locfxml);
-        System.out.flush();
-        
-        FXMLLoader fxmlLoader;
-    	try{
-    		fxmlLoader = new FXMLLoader(locfxml);
-    	}
-    	catch(Exception e){
-			System.out.printf("Attempting to load %s failed with: %s%n",loc,e.getMessage());
-			String sloc = "/" + loc;
-			locfxml = App.class.getResource(sloc);
-			fxmlLoader = new FXMLLoader(locfxml);
-    	}
-        return fxmlLoader.load();
-    }
+		}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	}
+
+	public void quit(Stage stage) {
+
+		stage.close();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.wit.cs.comp1050.javafx_maven_example {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens edu.wit.cs.comp1050.javafx_maven_example to javafx.fxml;
     exports edu.wit.cs.comp1050.javafx_maven_example;
